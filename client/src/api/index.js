137e033e5948021f8a61622ee6f515df202417cc
Cherif Merzoud <cherifmerzoud@MBP-de-Cherif.lan>
2021-06-29 21:07:32 +0200

    first commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://nouveau-blog.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://https://my-newblog.herokuapp.com/api/tasks' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
